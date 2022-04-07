2a8f88c460a5bb0f64d3bcfb5ab0face6cf3e527
epouyadou <epouyadou@kaliop.com>
2022-04-07 15:57:30 +0200

    front: WIP

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -13,17 +13,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.pressed)) {
+                  return const Color(0xFF5464B8);
+                }
+                return const Color(0xFF778AF1); // Use the component's default.
+              },
+            ),
+          ))),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -102,6 +111,12 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            ElevatedButton(
+              onPressed: (() {
+                _incrementCounter();
+              }),
+              child: Text('Press me'),
+            )
           ],
         ),
       ),
