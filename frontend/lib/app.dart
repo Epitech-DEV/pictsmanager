558e5dda47173df3cd628090c39b311db29b581e
epouyadou <epouyadou@kaliop.com>
2022-04-22 15:18:41 +0200

    front: enable material dark theme

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -8,15 +8,8 @@ class PictsManagerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Picts Manager',
-<<<<<<< HEAD
       theme: ThemeData.dark(),
-      home: const Home(),
-=======
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: const HomeView(),
->>>>>>> e991ee5e0e684b7c9e4b0b1d54f342d83fdb4f9f
     );
   }
 }
