6dd4d910c32e23882630332fbb10e38afbc44359
Arnaud <arnaud.roncari@epitech.eu>
2022-05-27 16:54:07 +0200

    Add delete picture

--- backend/lib/services/auth.dart
+++ backend/lib/services/auth.dart
@@ -6,17 +6,17 @@ import 'package:cobalt/core/service/backend_service.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 class AuthService with BackendServiceMixin {
-  DbCollection users = Mongo.db.collection('users');
+  DbCollection usersCollection = Mongo.db.collection('users');
 
   Future<String> register(String username, String password) async {
     final Map<String, dynamic>? doc =
-        await users.findOne({"username": username});
+        await usersCollection.findOne({"username": username});
     if (doc != null) {
       backend.throwError("auth:register:username:exist");
     }
     final User user =
         User(username: username, password: EncryptHelper.encrypt(password));
-    final WriteResult res = await users.insertOne(user.toJson());
+    final WriteResult res = await usersCollection.insertOne(user.toJson());
     final ObjectId id = res.document!["_id"] as ObjectId;
     final String jwt = backend.getService<JWTService>()!.generate(
           id.toHexString(),
@@ -26,7 +26,7 @@ class AuthService with BackendServiceMixin {
 
   Future<String> login(String username, String password) async {
     final Map<String, dynamic>? doc =
-        await users.findOne({"username": username});
+        await usersCollection.findOne({"username": username});
     final User? user = doc == null ? null : User.fromJson(doc);
     if (doc == null || !EncryptHelper.match(password, user!.password)) {
       backend.throwError("auth:login:invalid");
