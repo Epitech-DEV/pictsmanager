688e0c2320a64dd642bfe48d2948190ae1f43cb8
Arnaud <arnaud.roncari@epitech.eu>
2022-05-27 15:03:25 +0200

    Delete useless files

--- database/00-tables/01_users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.users (
-  id SERIAL PRIMARY KEY,
-  username TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-  CONSTRAINT unique_username UNIQUE (username)
-);
